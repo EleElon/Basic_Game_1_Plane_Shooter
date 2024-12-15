4718f8efb23add247708c26671feded095f113d0
EleElon <eleelon2309@gmail.com>
2024-12-16 01:31:50 +0700

    Pushing source
    Added moving feature of player

---
+++ Plane_Shooter/Assets/Script/Player.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Vector3 moveSpeed = Vector3.zero;
+    public float maxSpeed;
+    public float giaToc;
+    public float giaTocGiam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x_Posi = Input.GetAxisRaw("Horizontal");
+        float y_Posi = Input.GetAxisRaw("Vertical");
+        Vector3 vt3 = new Vector3(x_Posi, y_Posi, 0).normalized;
+
+        if ((transform.position.x <= -7.75 && x_Posi < 0) || (transform.position.x >= 6.5 && x_Posi > 0) || (transform.position.y >= -3.5 && y_Posi > 0) || (transform.position.y <= -6.71 && y_Posi < 0))
+        {
+            moveSpeed = Vector3.zero; 
+            return;
+        }
+        else
+        {
+            if (vt3.magnitude > 0)
+            {
+                moveSpeed = Vector3.MoveTowards(moveSpeed, vt3 * maxSpeed, giaToc * Time.deltaTime);
+            }
+            else
+            {
+                moveSpeed = Vector3.MoveTowards(moveSpeed, Vector3.zero, giaTocGiam * Time.deltaTime);
+            }
+        }
+
+        // transform.position += Vector3.right * moveSpeed * x_Posi * Time.deltaTime;
+        // transform.position += Vector3.up * moveSpeed * y_Posi * Time.deltaTime;
+        transform.Translate(moveSpeed * Time.deltaTime);
+    }
+}
